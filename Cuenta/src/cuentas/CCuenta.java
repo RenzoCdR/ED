0209e9ecf4295c2d900bec2b61dbd3d0a28eff2d
renzo <renzo@192.168.1.38>
2022-02-21 02:14:50 +0100

    Fin

--- Cuenta/src/cuentas/CCuenta.java
+++ Cuenta/src/cuentas/CCuenta.java
@@ -1,40 +1,108 @@
-public class CCuenta {
+package cuentas;
 
+/**
+ * En esta clase se muestran todos los métodos de la clase CCuenta
+ * Constructor sobrecargado, uno sin parámetros y otro con los parámetros nombre, cuenta, saldo y tipo de interés.
+ * Sus parámetros están encapsulados.
+ * Se puede consultar el estado, ingresar y retirar mediante sus correspondientes métodos.
+ * 
+ * @author renzo
+ *
+ */
+public class CCuenta {
 
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterés;
-
+    private double tipoInteres;
+    
+    /**
+     * Construye una CCuenta con valores nulos
+     */
     public CCuenta()
     {
     }
-
+    
+    /**
+     * Construye una CCuenta con los parámetros especificos
+     * @param nom nombre del titular de la cuenta
+     * @param cue nuemro de la cuenta
+     * @param sal saldo en la cuenta
+     * @param tipo Tipo de interés
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
+        setTipoInteres(tipo);
     }
-
+    
+    /**
+     * Comprueba el estado actual de la cuenta
+     * @return saldo de la cuenta
+     */
     public double estado()
     {
-        return saldo;
+        return getSaldo();
     }
 
+    /**
+     * Recibe la cantidad que hay que incrementar al saldo actual de la cuenta
+     * @param cantidad Saldo a incrementar
+     * @throws Exception Devuelve el mensaje si la cantidad es negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Recibe la cantidad que hay que restar del saldo actual
+     * @param cantidad Saldo a restar
+     * @throws Exception devuelve un mensaje si la cantidad a retirar es 0 o negativa y otro mensaje distinto si la cantidad a
+     * retirar es superior a la actual en cuenta
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
+
+	private String getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String getCuenta() {
+		return cuenta;
+	}
+
+	private void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	private double getSaldo() {
+		return saldo;
+	}
+
+	private void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	private double getTipoInteres() {
+		return tipoInteres;
+	}
+
+	private void setTipoInteres(double tipoInteres) {
+		this.tipoInteres = tipoInteres;
+	}
 }
--- Cuenta/src/cuentas/Main.java
+++ Cuenta/src/cuentas/Main.java
@@ -1,23 +1,51 @@
-public class Main {
+package cuentas;
 
+/**
+ * Programa principal que incluye el método main y el método operativa_cuenta
+ * Desde aquí se se realizarán las acciones para interactuar con las distintas clases.
+ * 
+ * @author renzo
+ *
+ */
+public class Main {
+	
+	/**
+	 * Método Main
+	 * Se declara y genera la cuenta y el saldo actual.
+	 * Imprime en pantalla un mensaje con el saldo actual de la cuenta.
+	 * Ejecuta el método operativa_cuenta
+	 *
+	 */
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta cuenta1;
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
-        try {
-            miCuenta.retirar(2300);
+        operativa_cuenta(cuenta1, 0.0f);
+    }
+    
+    /**
+     * En esté método se pretende realizar las operaciones de ingresar y retirar dinero de una cuenta y se indica mediante
+     * un mensaje si se ha realizado la operación o ha ocurrido un fallo.
+     * @param cuenta Cuenta sobre la que se van a realizar las operaciones
+     * @param cantidad Importe con el que se va a operar. (tipo float)
+     */
+    //TODO modoficar el tipo de la variable cantidad, en el resto del programa se utiliza double para los valores decimales.
+    
+	private static void operativa_cuenta(CCuenta cuenta, float cantidad) {
+		try {
+            cuenta.retirar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta.ingresar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
-    }
+	}
 }
